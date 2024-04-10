43c41c64a57b5e78959110eb3a9742ceedb320e6
Ashwinkumar Pillai <ashwin@Ashwinkumars-MacBook-Pro.local>
2024-04-10 03:30:38 -0400

    refactored imports and tested local pip package creation

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='ta_assignment_automation',
     version='0.1',
@@ -8,9 +11,11 @@ setup(
         "numpy",
         "pandas"
     ],
+    description='Assign TAs to Classes using a global optimization algorithm',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     license="MIT",
     author='Ashwin Pillai',
     author_email='ashwinkumarpillai1729@gmail.com',
-    description='Assign TAs to Classes using a global optimization algorithm',
     url='https://github.com/AshwinkumarPillai/TA_assignment_automation',
 )
